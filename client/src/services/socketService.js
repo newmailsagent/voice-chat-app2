c94fe68147c7832aeddaa4ac9a3386928cf135db
newmailsagent <norfang@gmail.com>
2025-10-07 20:27:19 +0300

    Add files via upload

--- client/src/services/socketService.js
+++ client/src/services/socketService.js
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 const SOCKET_URL = 'https://pobesedka.ru';
 
 export const socket = io(SOCKET_URL, {
-  transports: ['websocket', 'polling'], // ← разрешить fallback на polling
+  transports: ['polling'], // ← разрешить fallback на polling
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
