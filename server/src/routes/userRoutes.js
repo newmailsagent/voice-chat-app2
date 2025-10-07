d3b56403a641c97e6c8c391583951152e0195c39
newmailsagent <norfang@gmail.com>
2025-10-08 00:01:22 +0300

    Add files via upload

---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,9 @@
+// server/src/routes/userRoutes.js
+const express = require('express');
+const { getAllUsers } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.get('/', getAllUsers);
+
+module.exports = router;
