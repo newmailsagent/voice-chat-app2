7d94be00002b9bdc3de5b94980e576fc304d8205
newmailsagent <norfang@gmail.com>
2025-10-07 21:39:53 +0300

    Add files via upload

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -3,14 +3,15 @@ const bcrypt = require('bcrypt');
 const db = require('../db/database');
 const SALT_ROUNDS = 10;
 
-const createUser = (username, password) => {
+const createUser = async (username, password) => {
+  const userId = Math.random().toString(36).substring(2, 10) + Date.now().toString(36);
+  const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+
   return new Promise((resolve, reject) => {
-    const userId = Math.random().toString(36).substring(2, 10) + Date.now().toString(36);
-    const passwordHash = bcrypt.hashSync(password, SALT_ROUNDS);
-    
-    db.run('INSERT INTO users (id, username, passwordHash) VALUES (?, ?, ?)', 
+    db.run(
+      'INSERT INTO users (id, username, passwordHash) VALUES (?, ?, ?)',
       [userId, username, passwordHash],
-      function(err) {
+      function (err) {
         if (err) reject(err);
         else resolve({ id: userId, username });
       }
